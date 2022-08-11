a42aa06a3dbbfe2913be61e0e04020e315a335bd
Amirmasoud <amirmasood33@gmail.com>
2022-08-10 21:00:44 -0600

    WIP

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,10 +2,29 @@
 
 namespace Tests;
 
+use Pepper\PepperServiceProvider;
 use Orchestra\Testbench\TestCase as BaseTestCase;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Orchestra\Testbench\TestCase as Orchestra;
 
-class TestCase extends BaseTestCase
+class TestCase extends Orchestra
 {
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        Factory::guessFactoryNamesUsing(
+            fn (string $modelName) => 'VendorName\\Skeleton\\Database\\Factories\\'.class_basename($modelName).'Factory'
+        );
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [
+            PepperServiceProvider::class,
+        ];
+    }
+
     protected function getEnvironmentSetUp($app)
     {
         if (env('TESTS_ENABLE_LAZYLOAD_TYPES') === '1') {
